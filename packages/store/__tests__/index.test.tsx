d4281fdc4fa103aea4826ac5b48743149fca9b0a
Corbin Crutchley <git@crutchcorn.dev>
2023-08-31 09:58:11 -0700

    chore: add linting configuration files

--- packages/store/__tests__/index.test.tsx
+++ packages/store/__tests__/index.test.tsx
@@ -1,28 +1,28 @@
-import {describe, test, expect, vi} from 'vitest'
-import {Store} from "../src/index";
+import { describe, test, expect, vi } from 'vitest'
+import { Store } from '../src/index'
 
 describe('store', () => {
   test(`should set the initial value`, () => {
-    const store = new Store(0);
+    const store = new Store(0)
 
     expect(store.state).toEqual(0)
   })
 
   test(`basic subscriptions should work`, () => {
-    const store = new Store(0);
+    const store = new Store(0)
 
-    const subscription = vi.fn();
+    const subscription = vi.fn()
 
     const unsub = store.subscribe(subscription)
 
-    store.setState(() => 1);
+    store.setState(() => 1)
 
     expect(store.state).toEqual(1)
     expect(subscription).toHaveBeenCalled()
 
-    unsub();
+    unsub()
 
-    store.setState(() => 2);
+    store.setState(() => 2)
 
     expect(store.state).toEqual(2)
 
@@ -30,54 +30,54 @@ describe('store', () => {
   })
 
   test(`setState passes previous state`, () => {
-    const store = new Store(3);
+    const store = new Store(3)
 
-    store.setState((v) => v + 1);
+    store.setState((v) => v + 1)
 
     expect(store.state).toEqual(4)
   })
 
   test(`updateFn acts as state transformer`, () => {
     const store = new Store(1, {
-      updateFn: v => updater => Number(updater(v))
-    });
+      updateFn: (v) => (updater) => Number(updater(v)),
+    })
 
-    store.setState((v) => `${v + 1}` as never);
+    store.setState((v) => `${v + 1}` as never)
 
     expect(store.state).toEqual(2)
 
-    store.setState((v) => `${v + 2}` as never);
+    store.setState((v) => `${v + 2}` as never)
 
     expect(store.state).toEqual(4)
 
-    expect(typeof store.state).toEqual("number")
+    expect(typeof store.state).toEqual('number')
   })
 
-  test("Batch prevents listeners from being called during repeated setStates", () => {
-    const store = new Store(0);
+  test('Batch prevents listeners from being called during repeated setStates', () => {
+    const store = new Store(0)
 
-    const listener = vi.fn();
+    const listener = vi.fn()
 
-    store.subscribe(listener);
+    store.subscribe(listener)
 
     store.batch(() => {
-      store.setState(() => 1);
-      store.setState(() => 2);
-      store.setState(() => 3);
-      store.setState(() => 4);
+      store.setState(() => 1)
+      store.setState(() => 2)
+      store.setState(() => 3)
+      store.setState(() => 4)
     })
 
-    expect(store.state).toEqual(4);
+    expect(store.state).toEqual(4)
     // Listener is only called once because of batching
-    expect(listener).toHaveBeenCalledTimes(1);
+    expect(listener).toHaveBeenCalledTimes(1)
 
-    store.setState(() => 1);
-    store.setState(() => 2);
-    store.setState(() => 3);
-    store.setState(() => 4);
+    store.setState(() => 1)
+    store.setState(() => 2)
+    store.setState(() => 3)
+    store.setState(() => 4)
 
-    expect(store.state).toEqual(4);
+    expect(store.state).toEqual(4)
     // Listener is called 4 times because of a lack of batching
-    expect(listener).toHaveBeenCalledTimes(5);
+    expect(listener).toHaveBeenCalledTimes(5)
   })
 })
