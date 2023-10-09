cfda5c55fe0c456707c48367ca13247604e7c6f3
Robert Soriano <sorianorobertc@gmail.com>
2023-10-09 02:33:26 -0700

    feat: solid-js adapter (#25)
    * feat: add solid-js adapter
    
    * build: move solid-js dependency to root
    
    * update eslint and tsconfig opts
    
    * test(solid): add basic test
    
    * chore: remove extra log
    
    * test(solid): add setter test
    
    * test(solid): update setter test
    
    * test(solid): add not working test
    
    * test(solid): remove unused async keywords

---
+++ packages/solid-store/src/__tests__/index.test.tsx
@@ -0,0 +1,43 @@
+import { render, renderHook } from '@solidjs/testing-library'
+import '@testing-library/jest-dom'
+import { Store } from '@tanstack/store'
+import { useStore } from '../index'
+
+describe('useStore', () => {
+  it.todo('allows us to select state using a selector', async () => {
+    const store = new Store({
+      select: 0,
+      ignored: 1,
+    })
+
+    function Comp() {
+      const storeVal = useStore(store, (state) => state.select)
+
+      return <p>Store: {storeVal()}</p>
+    }
+
+    const { getByText } = render(() => <Comp />)
+    expect(getByText('Store: 0')).toBeInTheDocument()
+  })
+
+  it('allows us to select state using a selector', () => {
+    const store = new Store({
+      select: 0,
+      ignored: 1,
+    })
+
+    const { result } = renderHook(() => useStore(store, (state) => state.select))
+    
+    expect(result()).toBe(0)
+  })
+
+  it('updates accessor value when state is updated', () => {
+    const store = new Store(0)
+
+    const { result } = renderHook(() => useStore(store))
+    
+    store.setState((prev) => prev + 1)
+
+    expect(result()).toBe(1)
+  })
+})
