5700cc3e24fb2e07795f5ea0309a41873e10efb1
Corbin Crutchley <git@crutchcorn.dev>
2024-02-06 21:50:06 -0800

    chore: add initial benchmarks

---
+++ packages/store/src/tests/derived.bench.tsx
@@ -0,0 +1,84 @@
+import { Store } from '../store'
+import { Derived } from '../derived'
+import { describe, bench } from 'vitest'
+import { computed as vueComputed, ref, watch } from 'vue'
+import { createEffect, createMemo, createSignal } from 'solid-js'
+import { signal, computed as preactComputed, effect } from '@preact/signals'
+
+function noop(val: any) {
+  val
+}
+
+/**
+ *         A
+ *        / \
+ *       B   C
+ *      / \  |
+ *     D  E  F
+ *      \ / |
+ *       \ /
+ *        G
+ */
+describe('Derived', () => {
+  bench('TanStack', () => {
+    const a = new Store(1)
+    const b = new Derived([a], () => a.state)
+    const c = new Derived([a], () => a.state)
+    const d = new Derived([b], () => b.state)
+    const e = new Derived([b], () => b.state)
+    const f = new Derived([c], () => c.state)
+    const g = new Derived([d, e, f], () => d.state + e.state + f.state)
+
+    g.subscribe(() => noop(g.state))
+
+    a.setState(() => 2)
+  })
+
+  bench('Vue', () => {
+    const a = ref(1)
+    const b = vueComputed(() => a.value)
+    const c = vueComputed(() => a.value)
+    const d = vueComputed(() => b.value)
+    const e = vueComputed(() => b.value)
+    const f = vueComputed(() => c.value)
+    const g = vueComputed(() => d.value + e.value + f.value)
+
+    watch(g, () => {
+      noop(g.value)
+    })
+
+    a.value = 2
+  })
+
+  bench('Solid', () => {
+    const [a, setA] = createSignal(1)
+    const b = createMemo(() => a())
+    const c = createMemo(() => a())
+    const d = createMemo(() => b())
+    const e = createMemo(() => b())
+    const f = createMemo(() => c())
+    const g = createMemo(() => d() + e() + f())
+
+    createEffect(() => {
+      noop(g())
+    })
+
+    setA(2)
+  })
+
+  bench('Preact', () => {
+    const a = signal(1)
+    const b = preactComputed(() => a.value)
+    const c = preactComputed(() => a.value)
+    const d = preactComputed(() => b.value)
+    const e = preactComputed(() => b.value)
+    const f = preactComputed(() => c.value)
+    const g = preactComputed(() => d.value + e.value + f.value)
+
+    effect(() => {
+      noop(g.value)
+    })
+
+    a.value = 2
+  })
+})
