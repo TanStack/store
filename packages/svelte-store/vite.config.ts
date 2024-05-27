783a9ff8aec2859b28588169343c586c87a57ede
david <david.r-oe@gmx.de>
2024-05-27 12:41:34 +0200

    Move test setup file

--- packages/svelte-store/vite.config.ts
+++ packages/svelte-store/vite.config.ts
@@ -6,19 +6,21 @@ export default defineConfig({
   plugins: [sveltekit(), svelteTesting()],
   test: {
     alias: {
+      // This is needed for svelte-5 support
+      // https://github.com/testing-library/svelte-testing-library?tab=readme-ov-file#svelte-5-support
       '@testing-library/svelte': '@testing-library/svelte/svelte5',
     },
     name: 'svelte-store',
     dir: './src',
     watch: false,
     environment: 'jsdom',
-    setupFiles: ['test-setup.ts'],
+    setupFiles: ['./src/vitest.setup.ts'],
     coverage: {
-      // there seems to be some svelte-5 incompatibility so this is disabled for now
+      // There seems to be some svelte-5 incompatibility so this is disabled for now
       // e.g. https://cloud.nx.app/runs/q2YUKafLGK
       enabled: false,
       provider: 'istanbul',
-      include: ['src/**/*']
+      include: ['src/**/*'],
     },
     typecheck: { enabled: true },
     globals: true,
