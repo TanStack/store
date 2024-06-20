d01855a644a4b4486c99f6d5f47c57e9f97bbaf3
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-06-20 11:33:39 +1000

    Run prettier

--- packages/svelte-store/tests/Render.test.svelte
+++ packages/svelte-store/tests/Render.test.svelte
@@ -1,49 +1,44 @@
 <script lang="ts">
-    import { untrack } from 'svelte'
-    import { Store } from '@tanstack/store'
-    import { useStore } from "../src/index.svelte.js"
+  import { untrack } from 'svelte'
+  import { Store } from '@tanstack/store'
+  import { useStore } from '../src/index.svelte.js'
 
-    const store = new Store({
-      select: 0,
-      ignored: 1,
+  const store = new Store({
+    select: 0,
+    ignored: 1,
+  })
+
+  const storeVal = useStore(store, (state) => state.select)
+
+  let renderCount = $state(0)
+
+  $effect(() => {
+    storeVal.current
+    untrack(() => {
+      renderCount++
     })
-
-    const storeVal = useStore(store, (state) => state.select)
-
-
-    let renderCount = $state(0);
-
-
-    $effect(() => {
-      storeVal.current;
-      untrack(() => {
-        renderCount++;
-      });
-    });
+  })
 </script>
 
-
 <div>
-<p>Number rendered: {renderCount}</p>
-<p>Store: {storeVal.current}</p>
-<button
-  onclick={() =>
-    store.setState((v) => ({
-      ...v,
-      select: v.select + 10,
-    }))
-  }
->
-  Update select
-</button>
-<button
-  onclick={() =>
-    store.setState((v) => ({
-      ...v,
-      ignored: v.ignored + 10,
-    }))
-  }
->
-  Update ignored
-</button>
+  <p>Number rendered: {renderCount}</p>
+  <p>Store: {storeVal.current}</p>
+  <button
+    onclick={() =>
+      store.setState((v) => ({
+        ...v,
+        select: v.select + 10,
+      }))}
+  >
+    Update select
+  </button>
+  <button
+    onclick={() =>
+      store.setState((v) => ({
+        ...v,
+        ignored: v.ignored + 10,
+      }))}
+  >
+    Update ignored
+  </button>
 </div>
