8fa8ec352b871b121e2397429d51262a61a66331
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-06-20 14:24:31 +1000

    fix(angular-store): Use `ng-packagr` as build tool (#62)
    * Use ng-packagr
    
    * Simplify
    
    * Fix tsconfig
    
    * Fix knip

--- packages/angular-store/vite.config.ts
+++ packages/angular-store/vite.config.ts
@@ -1,24 +1,14 @@
-import { defineConfig, mergeConfig } from 'vitest/config'
-import { tanstackBuildConfig } from '@tanstack/config/build'
+import { defineConfig } from 'vitest/config'
 
-const config = defineConfig({
+export default defineConfig({
   test: {
     name: 'angular-store',
-    dir: './src',
+    dir: './tests',
     watch: false,
     environment: 'jsdom',
-    setupFiles: ['src/tests/test-setup.ts'],
+    setupFiles: ['tests/test-setup.ts'],
+    globals: true,
     coverage: { enabled: true, provider: 'istanbul', include: ['src/**/*'] },
     typecheck: { enabled: true },
-    globals: true,
   },
 })
-
-export default mergeConfig(
-  config,
-  tanstackBuildConfig({
-    entry: './src/index.ts',
-    srcDir: './src',
-    exclude: ['./src/tests'],
-  }),
-)
