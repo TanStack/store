18230797f541bbadbe27b72e849d3318818e9284
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-06-25 20:22:59 +1000

    Add additional svelte config

--- packages/svelte-store/eslint.config.js
+++ packages/svelte-store/eslint.config.js
@@ -1,11 +1,20 @@
 // @ts-check
 
+import tseslint from 'typescript-eslint'
 import pluginSvelte from 'eslint-plugin-svelte'
 import rootConfig from '../../eslint.config.js'
 
 export default [
   ...rootConfig,
   ...pluginSvelte.configs['flat/recommended'],
+  {
+    files: ['**/*.svelte'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
   {
     rules: {
       'svelte/no-svelte-internal': 'error',
