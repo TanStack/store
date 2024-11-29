dbbf6646d6264511cd94f3fabd30eb960142ae87
Corbin Crutchley <git@crutchcorn.dev>
2024-11-29 09:43:08 -0800

    chore: fix listener recieving old values

--- packages/store/src/store.ts
+++ packages/store/src/store.ts
@@ -49,10 +49,10 @@ export class Store<
   }
 
   setState = (updater: TUpdater) => {
-    const previous = this.state
+    this.prevState = this.state
     this.state = this.options?.updateFn
-      ? this.options.updateFn(previous)(updater)
-      : (updater as any)(previous)
+      ? this.options.updateFn(this.prevState)(updater)
+      : (updater as any)(this.prevState)
 
     // Always run onUpdate, regardless of batching
     this.options?.onUpdate?.()
