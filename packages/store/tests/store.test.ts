7a41b27ae2aeae60a6de2b3c5aa49f8bb69f16da
Corbin Crutchley <git@crutchcorn.dev>
2024-11-29 09:36:28 -0800

    chore: code cleanup and add back batch tests

--- packages/store/tests/store.test.ts
+++ packages/store/tests/store.test.ts
@@ -53,35 +53,6 @@ describe('store', () => {
     expect(typeof store.state).toEqual('number')
   })
 
-  test.skip('Batch prevents listeners from being called during repeated setStates', () => {
-    const store = new Store(0)
-
-    const listener = vi.fn()
-
-    const unsub = store.subscribe(listener)
-
-    // store.batch(() => {
-    //   store.setState(() => 1)
-    //   store.setState(() => 2)
-    //   store.setState(() => 3)
-    //   store.setState(() => 4)
-    // })
-
-    expect(store.state).toEqual(4)
-    // Listener is only called once because of batching
-    expect(listener).toHaveBeenCalledTimes(1)
-
-    store.setState(() => 1)
-    store.setState(() => 2)
-    store.setState(() => 3)
-    store.setState(() => 4)
-
-    expect(store.state).toEqual(4)
-    // Listener is called 4 times because of a lack of batching
-    expect(listener).toHaveBeenCalledTimes(5)
-    unsub()
-  })
-
   test('listeners should receive old and new values', () => {
     const store = new Store(12)
     const fn = vi.fn()
