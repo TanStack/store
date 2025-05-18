d4d21b0df6a1c95a14038800b96c7d7aff93f93a
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2025-05-18 10:42:27 +0330

    test: add failing test for the db package useStore failing.

--- packages/solid-store/tests/index.test.tsx
+++ packages/solid-store/tests/index.test.tsx
@@ -2,6 +2,7 @@ import { describe, expect, it } from 'vitest'
 import { render, renderHook } from '@solidjs/testing-library'
 import { Store } from '@tanstack/store'
 import { useStore } from '../src/index'
+import { createSignal } from 'solid-js'
 
 describe('useStore', () => {
   it.todo('allows us to select state using a selector', () => {
@@ -41,5 +42,20 @@ describe('useStore', () => {
     store.setState((prev) => prev + 1)
 
     expect(result()).toBe(1)
+  })
+
+  it('updates when updating to a new store in a signal', () => {
+    const { result } = renderHook(() => {
+      const [signal, setSignal] = createSignal({ store: new Store(0) })
+
+      return { value: useStore(signal().store), setSignal }
+    })
+
+    expect(result.value()).toBe(0)
+    result.setSignal((prev) => {
+      return { store: new Store(prev.store.state + 1) }
+    })
+
+    expect(result.value()).toBe(1)
   })
 })
