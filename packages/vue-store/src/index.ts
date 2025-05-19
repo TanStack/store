9dd35ec2700cc313629e94bd49b0fb1a39f07685
teleskop150750 <teleskop150750@gmail.com>
2025-05-19 10:23:43 +0300

    feat: MaybeRefOrGetter store

--- packages/vue-store/src/index.ts
+++ packages/vue-store/src/index.ts
@@ -1,6 +1,6 @@
-import { onScopeDispose, shallowRef } from 'vue'
+import { onWatcherCleanup, shallowRef, toValue, watchEffect } from 'vue'
 import type { Derived, Store } from '@tanstack/store'
-import type { Ref } from 'vue'
+import type { MaybeRefOrGetter, Ref } from 'vue'
 
 export * from '@tanstack/store'
 
@@ -10,27 +10,32 @@ export * from '@tanstack/store'
 export type NoInfer<T> = [T][T extends any ? 0 : never]
 
 export function useStore<TState, TSelected = NoInfer<TState>>(
-  store: Store<TState, any>,
+  store: MaybeRefOrGetter<Store<TState, any>>,
   selector?: (state: NoInfer<TState>) => TSelected,
 ): Readonly<Ref<TSelected>>
 export function useStore<TState, TSelected = NoInfer<TState>>(
-  store: Derived<TState, any>,
+  store: MaybeRefOrGetter<Derived<TState, any>>,
   selector?: (state: NoInfer<TState>) => TSelected,
 ): Readonly<Ref<TSelected>>
 export function useStore<TState, TSelected = NoInfer<TState>>(
-  store: Store<TState, any> | Derived<TState, any>,
+  store: MaybeRefOrGetter<Store<TState, any> | Derived<TState, any>>,
   selector: (state: NoInfer<TState>) => TSelected = (d) => d as any,
 ): Readonly<Ref<TSelected>> {
-  const slice = shallowRef(selector(store.state)) as Ref<TSelected>
+  const slice = shallowRef() as Ref<TSelected>
 
-  const unsub = store.subscribe(() => {
-    const newValue = selector(store.state)
-    if (shallow(slice.value, newValue)) return
+  watchEffect(() => {
+    const storeValue = toValue(store)
+    let sliceRef = selector(storeValue.state)
+    slice.value = sliceRef
+    const unsub = storeValue.subscribe(() => {
+      const newValue = selector(storeValue.state)
+      if (shallow(sliceRef, newValue)) return
+      sliceRef = newValue
+      slice.value = newValue
+    })
 
-    slice.value = newValue
+    onWatcherCleanup(unsub)
   })
-
-  onScopeDispose(unsub)
 
   return slice
 }
@@ -72,7 +77,7 @@ export function shallow<T>(objA: T, objB: T) {
 
   for (const keyA of keysA) {
     if (
-      !Object.prototype.hasOwnProperty.call(objB, keyA) ||
+      !Object.hasOwn(objB, keyA) ||
       !Object.is(objA[keyA as keyof T], objB[keyA as keyof T])
     ) {
       return false
