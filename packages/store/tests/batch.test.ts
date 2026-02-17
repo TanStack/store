cfb2ceba7241bbfdcc6337afb2b65293d878cea7
Manuel Schiller <meisterpink@gmail.com>
2026-02-17 08:07:30 +0100

    chore: add batch test (#191)
    * chore: add batch test
    
    * chore: update test to new API
    
    ---------
    
    Co-authored-by: Corbin Crutchley <git@crutchcorn.dev>

---
+++ packages/store/tests/batch.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, test } from 'vitest'
+import { batch, createStore } from '../src'
+
+describe('batch', () => {
+  test('updates store immediately', () => {
+    const store = createStore({ hello: 100, world: 200 })
+    batch(() => {
+      store.setState((state) => ({ ...state, hello: state.hello + 1 }))
+      expect(store.state.hello).toBe(101)
+      expect(store.state.world).toBe(200)
+      store.setState((state) => ({ ...state, world: state.world + 1 }))
+      expect(store.state.hello).toBe(101)
+      expect(store.state.world).toBe(201)
+    })
+
+    expect(store.state.hello).toBe(101)
+    expect(store.state.world).toBe(201)
+  })
+})
