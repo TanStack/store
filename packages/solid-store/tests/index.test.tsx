4c5120f7811f7462546ae48b63052b38599f0f91
Harry Whorlow <whorlowharry@gmail.com>
2026-04-28 13:11:33 +0200

    chore: update tests

--- packages/solid-store/tests/index.test.tsx
+++ packages/solid-store/tests/index.test.tsx
@@ -4,7 +4,7 @@ import { createAtom, createStore } from '@tanstack/store'
 import {
   _useStore,
   createStoreContext,
-  shallow,
+  evaluate,
   useAtom,
   useSelector,
   useStore,
@@ -263,101 +263,101 @@ describe('shallow', () => {
   test('should return true for shallowly equal objects', () => {
     const objA = { a: 1, b: 'hello' }
     const objB = { a: 1, b: 'hello' }
-    expect(shallow(objA, objB)).toBe(true)
+    expect(evaluate(objA, objB)).toBe(true)
   })
 
   test('should return false for objects with different values', () => {
     const objA = { a: 1, b: 'hello' }
     const objB = { a: 2, b: 'world' }
-    expect(shallow(objA, objB)).toBe(false)
+    expect(evaluate(objA, objB)).toBe(false)
   })
 
   test('should return false for objects with different keys', () => {
     const objA = { a: 1, b: 'hello' }
     const objB = { a: 1, c: 'world' }
     // @ts-expect-error
-    expect(shallow(objA, objB)).toBe(false)
+    expect(evaluate(objA, objB)).toBe(false)
   })
 
   test('should return false for objects with different structures', () => {
     const objA = { a: 1, b: 'hello' }
     const objB = [1, 'hello']
     // @ts-expect-error
-    expect(shallow(objA, objB)).toBe(false)
+    expect(evaluate(objA, objB)).toBe(false)
   })
 
   test('should return false for one object being null', () => {
     const objA = { a: 1, b: 'hello' }
     const objB = null
-    expect(shallow(objA, objB)).toBe(false)
+    expect(evaluate(objA, objB)).toBe(false)
   })
 
   test('should return false for one object being undefined', () => {
     const objA = { a: 1, b: 'hello' }
     const objB = undefined
-    expect(shallow(objA, objB)).toBe(false)
+    expect(evaluate(objA, objB)).toBe(false)
   })
 
   test('should return true for two null objects', () => {
     const objA = null
     const objB = null
-    expect(shallow(objA, objB)).toBe(true)
+    expect(evaluate(objA, objB)).toBe(true)
   })
 
   test('should return false for objects with different types', () => {
     const objA = { a: 1, b: 'hello' }
     const objB = { a: '1', b: 'hello' }
     // @ts-expect-error
-    expect(shallow(objA, objB)).toBe(false)
+    expect(evaluate(objA, objB)).toBe(false)
   })
 
   test('should return true for shallow equal objects with symbol keys', () => {
     const sym = Symbol.for('key')
     const objA = { [sym]: 1 }
     const objB = { [sym]: 1 }
-    expect(shallow(objA, objB)).toBe(true)
+    expect(evaluate(objA, objB)).toBe(true)
   })
 
   test('should return false for shallow different values for symbol keys', () => {
     const sym = Symbol.for('key')
     const objA = { [sym]: 1 }
     const objB = { [sym]: 2 }
-    expect(shallow(objA, objB)).toBe(false)
+    expect(evaluate(objA, objB)).toBe(false)
   })
 
   test('should return true for shallowly equal maps', () => {
     const objA = new Map([['1', 'hello']])
     const objB = new Map([['1', 'hello']])
-    expect(shallow(objA, objB)).toBe(true)
+    expect(evaluate(objA, objB)).toBe(true)
   })
 
   test('should return false for maps with different values', () => {
     const objA = new Map([['1', 'hello']])
     const objB = new Map([['1', 'world']])
-    expect(shallow(objA, objB)).toBe(false)
+    expect(evaluate(objA, objB)).toBe(false)
   })
 
   test('should return true for shallowly equal sets', () => {
     const objA = new Set([1])
     const objB = new Set([1])
-    expect(shallow(objA, objB)).toBe(true)
+    expect(evaluate(objA, objB)).toBe(true)
   })
 
   test('should return false for sets with different values', () => {
     const objA = new Set([1])
     const objB = new Set([2])
-    expect(shallow(objA, objB)).toBe(false)
+    expect(evaluate(objA, objB)).toBe(false)
   })
 
   test('should return false for dates with different values', () => {
     const objA = new Date('2025-04-10T14:48:00')
     const objB = new Date('2025-04-10T14:58:00')
-    expect(shallow(objA, objB)).toBe(false)
+    expect(evaluate(objA, objB)).toBe(false)
   })
 
   test('should return true for equal dates', () => {
     const objA = new Date('2025-02-10')
     const objB = new Date('2025-02-10')
-    expect(shallow(objA, objB)).toBe(true)
+    expect(evaluate(objA, objB)).toBe(true)
   })
 })
