850b112b172ddf27d2f5b31489675c961f4a50ac
Benjamín Vicente <projects@benjavicente.dev>
2026-09-20 13:21:55 -0300

    chore(angular): Angular 22 and native Vitest unit-test runner
    Align the adapter on Angular 22, switch tests to the @angular/build
    unit-test runner, and tidy related ignore/knip config.

--- packages/angular-store/tests/test-setup.ts
+++ packages/angular-store/tests/test-setup.ts
@@ -1,12 +1,1 @@
-import '@analogjs/vite-plugin-angular/setup-vitest'
-
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting,
-} from '@angular/platform-browser-dynamic/testing'
-import { getTestBed } from '@angular/core/testing'
-
-getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(),
-)
+import '@testing-library/jest-dom/vitest'
